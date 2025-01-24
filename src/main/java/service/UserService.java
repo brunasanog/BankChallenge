8197df8c2d9d3ab6f37ba2f307eb2a9c25e7388e
Bruna Sanog <brunasanog.contato@gmail.com>
2025-01-24 20:49:55 -0300

    Removendo comentários desnecessários na classe UserService

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -8,41 +8,11 @@ import java.time.LocalDate;
 
 public class UserService {
 
-    private final AuthService authService = new AuthService();
     private final UserDAO userDAO = new UserDAOImplements();
 
     public boolean createUser(String cpf, String name, String email, String phone, LocalDate birthDate, String accountType, String password) {
 
-        /* Valida o formato do CPF
-        String cpfFormatError = authService.validateCpfFormat(cpf);
-        if (cpfFormatError != null) {
-            System.out.println(cpfFormatError);
-            return false;
-        }
-
-        // Valida se o CPF já existe
-        String cpfError = authService.isCpfRegistered(cpf);
-        if (cpfError != null) {
-            System.out.println(cpfError);
-            return false;
-        }
-
-        // Validação de tipo de conta
-        String accountTypeError = authService.isValidAccountType(accountType);
-        if (accountTypeError != null) {
-            System.out.println(accountTypeError);
-            return false;
-        }
-
-        // Validação do formato da data
-        if (birthDate == null) {
-            System.out.println("Invalid birth date.");
-            return false;
-        }
-        */
-
-
-        // Criar objeto User
+        // CREATE USER
         User user = new User(
                 cpf,
                 name,
