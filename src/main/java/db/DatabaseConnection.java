38f220da46405e118f2650de7172c740f1f94abf
Bruna Sanog <brunasanog.contato@gmail.com>
2025-01-24 21:17:57 -0300

    Corrigindo erro de dados sensíveis do banco de dados

--- src/main/java/db/DatabaseConnection.java
+++ src/main/java/db/DatabaseConnection.java
@@ -4,12 +4,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class DatabaseConnection {
 
     //connection
-    private static final String URL = "jdbc:mysql://localhost:3306/bank_challenge";
-    private static final String USER = "root";
-    private static final String PASSWORD = "1254";
+    private static final Dotenv dotenv = Dotenv.load();
+    private static final String URL = dotenv.get("DB_URL");
+    private static final String USER = dotenv.get("DB_USER");
+    private static final String PASSWORD = dotenv.get("DB_PASSWORD");
 
     //get the connection to the database
     public static Connection getConnection() throws SQLException {
