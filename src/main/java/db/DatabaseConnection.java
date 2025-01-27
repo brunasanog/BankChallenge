b74ae3f3d06a8a7a38c0b5a094cd4d3306fbfa0f
Bruna Sanog <brunasanog.contato@gmail.com>
2025-01-27 11:23:59 -0300

    Retirando teste de conexao da classe database

--- src/main/java/db/DatabaseConnection.java
+++ src/main/java/db/DatabaseConnection.java
@@ -8,29 +8,18 @@ import io.github.cdimascio.dotenv.Dotenv;
 
 public class DatabaseConnection {
 
-    //connection
+    //CONNECTION
     private static final Dotenv dotenv = Dotenv.load();
     private static final String URL = dotenv.get("DB_URL");
     private static final String USER = dotenv.get("DB_USER");
     private static final String PASSWORD = dotenv.get("DB_PASSWORD");
 
-    //get the connection to the database
+    //GET CONNECTION
     public static Connection getConnection() throws SQLException {
         try {
             return DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (SQLException e) {
             throw new SQLException("Failed to establish a database connection. Please check your database URL, username, or password. Error details: " + e.getMessage());
-        }
-    }
-
-    //test the connection
-    public static void main(String[] args) {
-        try (Connection connection = getConnection()) {
-            if (connection != null) {
-                System.out.println("Connection established!");
-            }
-        } catch (SQLException e) {
-            System.out.println("Failed to establish a database connection. Please check your database URL, username, or password. Error details: " + e.getMessage());
         }
     }
 }
